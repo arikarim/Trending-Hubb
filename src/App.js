67c8a9eb1a442516d8f6627bc78ea244eaf382af
arikarim <arikarim146@gmail.com>
2021-06-06 12:06:27 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
 import SimpleBottomNavigation from './Components/MainNav';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <div className="app">
+        <Container>
+          <Switch>
+            <Rout path='/' component={Trending} />
+            <Rout path='/' component={Movies} />
+            <Rout path='/' component={Series} />
+            <Rout path='/' component={Search} />
+          </Switch>
+        </Container>
       </div>
       <SimpleBottomNavigation />
-    </>
+    </BrowserRouter>
   );
 }
 
