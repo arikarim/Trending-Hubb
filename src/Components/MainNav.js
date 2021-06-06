e11e76b3e6f33c67d9361edc23965966d1d5db0f
arikarim <arikarim146@gmail.com>
2021-06-06 11:59:34 +0300

    Create navbar

--- src/Components/MainNav.js
+++ src/Components/MainNav.js
@@ -5,10 +5,18 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import MovieIcon from '@material-ui/icons/Movie';
+import SearchIcon from '@material-ui/icons/Search';
+import TvIcon from '@material-ui/icons/Tv';
 
 const useStyles = makeStyles({
   root: {
-    width: 500,
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
+    backgroundColor: "#2d313a",
+    zIndex: 100,
   },
 });
 
@@ -25,9 +33,22 @@ export default function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction
+        style={{color: "white"}}
+        label="Trending"
+        icon={<WhatshotIcon />} />
+      <BottomNavigationAction 
+        style={{color: "white"}}
+        label="Movies"
+        icon={<MovieIcon />} />
+      <BottomNavigationAction
+        style={{color: "white"}}
+        label="TV Series"
+        icon={<TvIcon />} />
+        <BottomNavigationAction
+        style={{color: "white"}}
+        label="Search"
+        icon={<SearchIcon />} />
     </BottomNavigation>
   );
 }
