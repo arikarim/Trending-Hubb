4ed3077cea5ddc2c4645c7398c6eed596cb199b4
arikarim <arikarim146@gmail.com>
2021-06-06 13:23:50 +0300

    Add .env and fething

---
+++ src/Components/TrendContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TrendContent = ({data}) => {
+  return (
+    <div>
+      {data.name}
+    </div>
+  )
+}
+
+TrendContent.propTypes = {
+
+}
+
+export default TrendContent
--- src/Pages/Trending.js
+++ src/Pages/Trending.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
+import TrendContent from '../Components/TrendContent';
 
 const Trending = props => {
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`);
+    setContent(data.results);
+  }
+
+  useEffect(() => {
+   fetchTrending()
+  }, [])
+
   return (
-    <span className='pagesTitle'>Trending</span>
+    <div>
+      <span className='pagesTitle'>Trending</span>
+      <div className="trending">
+        {
+          content && content.map((c,key) => {
+            <TrendContent data={c} />
+          })
+        }
+      </div>
+    </div>
   )
 }
 
