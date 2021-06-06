f22ede20cc53930475365e7364b8a5867df24c7b
arikarim <arikarim146@gmail.com>
2021-06-06 21:16:45 +0300

    Featch movies

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
+import Paginationn from '../Components/Pagination/Paginationn';
+import TrendContent from '../Components/TrendContent';
 
 const Movies = props => {
   const [page, setPage] = useState(1);
@@ -11,15 +13,33 @@ const Movies = props => {
   const fetchMovies = async () => {
     const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&page=${page}`)
     
-    setContent(data.result);
+    setContent(data.results);
     setNumOfPages(data.total_pages)
   }
 
   useEffect(() => {
     fetchMovies()
-  }, [])
+  }, [page])
   return (
-    <span className='pagesTitle'>Movies</span>
+    <div>
+
+      <span className='pagesTitle'>Movies</span>
+      <div className="trending">
+          {
+            content && content.map((c, key) =>
+             <TrendContent
+              key={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name} 
+              date={c.first_air_date || c.release_date}
+              media_type='Movie'
+              vote_average={c.vote_average} />)
+          }
+        </div>
+        {numOfPages > 1 &&
+        <Paginationn setPage={setPage} numOfPages={numOfPages} />
+        }
+    </div>
   )
 }
 
