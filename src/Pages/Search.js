29080d1d8b62c644fe992ff61b2b987ed17aa194
arikarim <arikarim146@gmail.com>
2021-06-07 18:09:12 +0300

    Add Search component

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { Button, createMuiTheme, Tabs, TextField, ThemeProvider } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
 const Search = props => {
+  const [type, setType] = useState(0)
+
+
+const darkTheme = createMuiTheme({
+  palette:{
+    type: 'dark',
+    primary: {
+      main: "#fff",
+    }
+  }
+})
   return (
-    <span className='pagesTitle'>Search</span>
+    
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <div style={{display: 'flex',margin:"15px 0"}}>
+          <TextField 
+            style={{flex:1}} 
+            label="search"
+            variant="filled"
+            // onChange={(e) => setSearchText(e.target.value)} 
+            />
+
+          
+          <Button variant="contained" style={{marginLeft: 10}} >
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <Tabs value={type}>
+
+        </Tabs>
+      </ThemeProvider>
+    </div>
   )
 }
 
