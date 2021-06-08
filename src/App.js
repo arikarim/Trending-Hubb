26ad4e4f8b2dfb022b9faa7243390a9a60ddd2ef
arikarim <arikarim146@gmail.com>
2021-06-08 16:46:56 +0300

    Update title

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             <Route path='/movies' component={Movies} />
             <Route path='/series' component={Series} />
             <Route path='/search' component={Search} />
-            <Route path='/' component={Trending} />
+            <Route path='/' exact component={Trending} />
           </Switch>
         </Container>
       </div>
