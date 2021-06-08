b35b64ae6e356b6df52f8a6fc928762858e5c34f
arikarim <arikarim146@gmail.com>
2021-06-08 14:05:29 +0300

    Add style to carousel

--- src/Components/carousel/Carousel.js
+++ src/Components/carousel/Carousel.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { img_300, noPicture } from '../../config/config';
+import "./Carousel.css";
 
 const handleDragStart = (e) => e.preventDefault();
 
@@ -12,7 +14,20 @@ const items = [
 ];
 
  const Gallery = ({media_type, id}) => {
-  const [credits, setCredits] = useState()
+  const [credits, setCredits] = useState();
+
+  const items = credits.map((c) => (
+    <div className="carouselItem">
+      <img
+        src={c.profile_path ? `${img_300}/${c.profile_path}` : noPicture}
+        alt={c?.name}
+        onDragStart={handleDragStart}
+        className="carouselItem__img"
+      />
+      <b className="carouselItem__txt">{c?.name}</b>
+    </div>
+  ));
+
   const fetchCredits = async () => {
     const { data } = await axios.get(`https://api.themoviedb.org/3/${ media_type }/${id}/credits?api_key=${process.env.REACT_APP_API_KEY}&lanfuage=en-US`)
     
