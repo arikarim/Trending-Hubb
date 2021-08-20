f2f883639c53046fa610077d52f06b77f72b0eec
arikarim <arikarim146@gmail.com>
2021-08-20 10:14:07 +0300

    Fix card heights

--- src/Model/Model.js
+++ src/Model/Model.js
@@ -77,8 +77,8 @@ export default function Model({ children, media_type, id }) {
   }, []);
 
   return (
-    <div className="col-10 col-md-6 col-lg-3">
-      <div className="media  mx-auto" type="button" onClick={handleOpen}>
+    <div className="col-10 px-md-3 col-md-6 col-lg-3">
+      <div className="media mx-auto" type="button" onClick={handleOpen}>
         {children}
       </div>
       <Modal
