51db55ee1408c3b4aebbee7217df6f045811ad68
arikarim <arikarim146@gmail.com>
2021-08-20 19:18:41 +0300

    Add PWA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorkerRegistration.register();
